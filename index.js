8c8f53d9df74e39726ec4ec2df6ddfcd9daea005
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-01-18 09:27:08 +0530

    add

--- index.js
+++ index.js
@@ -2,20 +2,24 @@
 // We will understand all of this later in the course.
 // DO NOT MODIFY THIS FILE
 
-const express = require('express');
-const path = require('path');
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
+// const app = express();
 
-app.use(express.static(__dirname))
+// app.use(express.static(__dirname))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/main.html'));
+// });
+// //your code here
+// app.post('/add', (req, res) => {
+//   const {a,b} = req.body;
+//   res.status(200).send(a+b);
+//   // res.sendFile(path.join(__dirname + '/main.html'));
+// });
+// module.exports = app;
+const app = require("./index.js");
+app.listen(3000, () => {
+  console.log('server started');
 });
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
