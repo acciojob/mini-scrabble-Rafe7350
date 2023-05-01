8d0d5df3afd0dd173cf3eb8ff742034cfabed627
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-05-01 19:35:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function countChars(obj) {
-  document.getElementById("letterCount").innerHTML = obj.value.length;
-}
+// function countChars(obj) {
+//   document.getElementById("letterCount").innerHTML = obj.value.length;
+// }
+let evaluatedText = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
+
+evaluatedText.addEventListener('input', () => {
+	let str = evaluatedText.value;
+	letterCount.innerHTML = `${str.length}`;
+});
